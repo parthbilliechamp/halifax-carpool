f1da2fee42abe8ceff27877d08376975880b19a5
parthchampaneria <parthchamp169@gmail.com>
2022-10-05 17:36:43 -0300

    removing springboot annotation from test

--- src/test/java/com/halifaxcarpool/HalifaxCarpoolApplicationTests.java
+++ src/test/java/com/halifaxcarpool/HalifaxCarpoolApplicationTests.java
@@ -3,7 +3,7 @@ package com.halifaxcarpool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class HalifaxCarpoolApplicationTests {
 
 	@Test
