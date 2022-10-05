dcacf243ad9c47fe86e84c3a880fb32bb4dcf095
parthchampaneria <parthchamp169@gmail.com>
2022-10-04 23:53:18 -0300

    spring boot configuration

---
+++ src/main/java/com/halifaxcarpool/HalifaxCarpoolApplication.java
@@ -0,0 +1,13 @@
+package com.halifaxcarpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalifaxCarpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalifaxCarpoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/halifaxcarpool/HalifaxCarpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halifaxcarpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalifaxCarpoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
