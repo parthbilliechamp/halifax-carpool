db1018a032d274148569591afb7471d35b11a8e3
parthchampaneria <parthchamp169@gmail.com>
2022-10-07 00:31:14 -0300

    commenting ServletInitializer.java

--- src/main/java/com/halifaxcarpool/ServletInitializer.java
+++ src/main/java/com/halifaxcarpool/ServletInitializer.java
@@ -3,11 +3,11 @@ package com.halifaxcarpool;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(HalifaxCarpoolApplication.class);
-	}
-
-}
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(HalifaxCarpoolApplication.class);
+//	}
+//
+//}
