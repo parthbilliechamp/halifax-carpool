37c8a87f74a32c51338fbbc9989b6b7e30aa98ee
parthchampaneria <parthchamp169@gmail.com>
2022-10-06 23:20:26 -0300

    added controller

--- src/main/java/com/halifaxcarpool/HalifaxCarpoolApplication.java
+++ src/main/java/com/halifaxcarpool/HalifaxCarpoolApplication.java
@@ -2,9 +2,20 @@ package com.halifaxcarpool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class HalifaxCarpoolApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HalifaxCarpoolApplication.class, args);
