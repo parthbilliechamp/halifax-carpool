0aef27ea73c1004fa4433d18cf989fedcdc0c007
parthchampaneria <parthchamp169@gmail.com>
2022-10-07 00:57:54 -0300

    configuration

--- src/main/java/com/halifaxcarpool/HalifaxCarpoolApplication.java
+++ src/main/java/com/halifaxcarpool/HalifaxCarpoolApplication.java
@@ -2,18 +2,26 @@ package com.halifaxcarpool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
+@Configuration
 public class HalifaxCarpoolApplication {
 
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
 		return "Hello World!";
+	}
+
+	@RequestMapping("/check")
+	@ResponseBody
+	String check() {
+		return "check!";
 	}
 
 
