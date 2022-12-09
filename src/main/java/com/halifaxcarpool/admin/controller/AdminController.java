880551a70e7bdfc09ba3bcf6966510888a9fe6ef
Taha Mustufa Zanzibarwala <th803624@dal.ca>
2022-12-08 22:31:29 -0400

    integration to front end complete. Feature complete

--- src/main/java/com/halifaxcarpool/admin/controller/AdminController.java
+++ src/main/java/com/halifaxcarpool/admin/controller/AdminController.java
@@ -6,12 +6,16 @@ import com.halifaxcarpool.admin.business.statistics.UserAnalysis;
 import com.halifaxcarpool.admin.business.statistics.UserStatistics;
 import com.halifaxcarpool.admin.database.dao.IUserDetails;
 import com.halifaxcarpool.driver.database.dao.DriverDetailsDaoImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AdminController {
+
+    private static final String DRIVER_STATISTICS = "view_driver_stats";
 
     @GetMapping("/admin")
     @ResponseBody
@@ -19,12 +23,15 @@ public class AdminController {
         return "Hello World!";
     }
 
-    @GetMapping("/admin/viewDriverStatistics")
-    public void showDriverStatistic(){
+    @GetMapping("/admin/view_driver_stats")
+    public String showDriverStatistic(Model model){
         IUserDetails driverDetails = new DriverDetailsDaoImpl();
         IUserStatisticsBuilder userStatisticsBuilder = new DriverStatistics(driverDetails);
         UserStatistics userStatistics = new UserAnalysis(userStatisticsBuilder).deriveDriverStatistics();
 
+        model.addAttribute("userStats", userStatistics);
+
+        return DRIVER_STATISTICS;
     }
 
 }
